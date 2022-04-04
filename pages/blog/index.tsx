7b475c56f827411c00e2fc2a9c3d552656c5fd6f
xiraynedev <xiraynedev@gmail.com>
2022-04-04 15:50:13 -0400

    add server-side authentication for blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,28 @@
 import {FC} from 'react';
+import {GetServerSideProps} from 'next';
+import {getSession} from 'next-auth/react';
 
-const Index: FC = () => {
+interface BlogProps {
+  data: string;
+}
+
+const Blog: FC<BlogProps> = ({data}) => {
   return (
-    <div>
+    <div className='flex flex-col gap-5'>
       <h1 className='text-3xl text-green-500'>Blog page</h1>
+      <h2 className='text-2xl text-green-800'>{data}</h2>
     </div>
   )
 }
 
-export default Index;
+export default Blog;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+      data: session ? 'List of Star Wars Characters' : 'Sign in to see your personalized characters list',
+    },
+  };
+};
