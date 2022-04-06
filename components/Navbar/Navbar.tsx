419e4363e858458e7be56433e3b033125b7d8145
xiraynedev <xiraynedev@gmail.com>
2022-04-06 11:30:08 -0400

    refactor code in navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -17,38 +17,39 @@ export const Navbar: FC = () => {
   };
 
   return (
-    <nav className="p-6 shadow-lg flex items-center justify-between mb-4">
+    <nav className='p-6 shadow-lg flex items-center justify-between mb-4'>
       <div>
-        <Link href="/" passHref>
-          <h2 className="cursor-pointer text-3xl font-bold text-blue-500">next-demo</h2>
+        <Link href='/' passHref>
+          <h2 className='cursor-pointer text-3xl font-bold text-blue-500'>next-demo</h2>
         </Link>
       </div>
-      <div className="flex text-blue-400 font-bold">
+      <div className='flex text-blue-400 font-bold'>
         <button
           onClick={handleShowMenu}
-          className="text-2xl md:hidden">Menu
+          className='text-2xl md:hidden'>Menu
         </button>
-        <div className={`${showMenu ? 'translate-x-0' : 'translate-x-full'} transition duration-300 md:translate-x-0 absolute border md:static flex flex-col md:flex-row md:border-0
+        <div className={`${showMenu ? 'translate-x-0' : 'translate-x-full'} transition duration-300 md:translate-x-0
+        absolute border md:static flex flex-col md:flex-row md:border-0
         top-0 right-0 p-8 w-7/12 md:w-full h-screen md:h-fit gap-5 bg-white`}>
           <button
             onClick={handleCloseMenu}
-            className="absolute right-8 top-2 text-2xl md:hidden">X
+            className='absolute right-8 top-2 text-2xl md:hidden'>X
           </button>
-          <Link href="/">
-            <a className="hover:text-blue-300">HOME</a>
+          <Link href='/'>
+            <a className='hover:text-blue-300'>HOME</a>
           </Link>
           {
             session &&
-						<Link href="/dashboard">
-							<a className="hover:text-blue-300">DASHBOARD</a>
+						<Link href='/dashboard'>
+							<a className='hover:text-blue-300'>DASHBOARD</a>
 						</Link>
           }
-          <Link href="/blog">
-            <a className="hover:text-blue-300">BLOG</a>
+          <Link href='/blog'>
+            <a className='hover:text-blue-300'>BLOG</a>
           </Link>
           <Link href={!session ? '/api/auth/signin' : '/api/auth/signout'}>
             <a
-              className="hover:text-blue-300"
+              className='hover:text-blue-300'
               onClick={event => {
                 event.preventDefault();
                 if (!session) {
